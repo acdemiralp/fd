7eee73b274e59de57c97c42914e2f0db284405b4
acdemiralp <demiralpali@gmail.com>
2022-08-18 02:23:24 +0200

    Progress.

---
+++ tests/finite_differences_test.cpp
@@ -0,0 +1,28 @@
+#include "doctest/doctest.h"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include <fd/all.hpp>
+
+TEST_CASE("finite_differences")
+{
+  constexpr auto f = [ ] (const float x) { return std::sin(x); };
+  REQUIRE(fd::forward_difference <1>(f, static_cast<float>(0.0f              ), 0.001f) == doctest::Approx( std::cos(0.0f              )).epsilon(0.001));
+  REQUIRE(fd::forward_difference <1>(f, static_cast<float>(M_PI * 1.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 1.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::forward_difference <1>(f, static_cast<float>(M_PI * 2.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 2.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::forward_difference <1>(f, static_cast<float>(M_PI * 3.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 3.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::forward_difference <1>(f, static_cast<float>(M_PI              ), 0.001f) == doctest::Approx( std::cos(M_PI              )).epsilon(0.001));
+
+  REQUIRE(fd::backward_difference<1>(f, static_cast<float>(0.0f              ), 0.001f) == doctest::Approx( std::cos(0.0f              )).epsilon(0.001));
+  REQUIRE(fd::backward_difference<1>(f, static_cast<float>(M_PI * 1.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 1.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::backward_difference<1>(f, static_cast<float>(M_PI * 2.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 2.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::backward_difference<1>(f, static_cast<float>(M_PI * 3.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 3.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::backward_difference<1>(f, static_cast<float>(M_PI              ), 0.001f) == doctest::Approx( std::cos(M_PI              )).epsilon(0.001));
+
+  REQUIRE(fd::central_difference <1>(f, static_cast<float>(0.0f              ), 0.001f) == doctest::Approx( std::cos(0.0f              )).epsilon(0.001));
+  REQUIRE(fd::central_difference <1>(f, static_cast<float>(M_PI * 1.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 1.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::central_difference <1>(f, static_cast<float>(M_PI * 2.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 2.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::central_difference <1>(f, static_cast<float>(M_PI * 3.0f / 4.0f), 0.001f) == doctest::Approx( std::cos(M_PI * 3.0f / 4.0f)).epsilon(0.001));
+  REQUIRE(fd::central_difference <1>(f, static_cast<float>(M_PI              ), 0.001f) == doctest::Approx( std::cos(M_PI              )).epsilon(0.001));
+}
